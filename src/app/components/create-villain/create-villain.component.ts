abf163c65505ffbdaa5cbd26d5da70e2aa74325a
iancarpenter <ianhcarpenter@gmail.com>
2020-02-03 20:59:31 +0000

    Added create and error log messages

--- src/app/components/create-villain/create-villain.component.ts
+++ src/app/components/create-villain/create-villain.component.ts
@@ -1,23 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { HttpClientVillainService } from 'src/app/services/http-client-villain.service';
 import { Router } from "@angular/router";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-villain',
   templateUrl: './create-villain.component.html',
   styleUrls: ['./create-villain.component.css']
 })
-export class CreateVillainComponent {
-
+export class CreateVillainComponent implements OnDestroy {
+  
   createVillainForm = new FormGroup({
     id: new FormControl(''),
     name: new FormControl(''),
     episode: new FormControl('')
   });
 
+  villainSubscription: Subscription;
+
   constructor(private villainService: HttpClientVillainService,
               private router: Router) { }
+
+  ngOnDestroy(): void {
+    this.villainSubscription.unsubscribe;
+  }              
 
   createVillain() {
 
@@ -27,7 +34,10 @@ export class CreateVillainComponent {
       const name = this.createVillainForm.get('name').value;
       const episode = this.createVillainForm.get('episode').value;
 
-      this.villainService.addVillain(id, name, episode).subscribe();
+      this.villainSubscription = this.villainService.addVillain(id, name, episode).subscribe({
+        complete() { console.log('Villain created successfully');},
+        error(msg) { console.log('Error trying to create villain', msg); }
+      });
 
       this.router.navigateByUrl("/");
     }
